f074b2d6999bd4341e8ceecc95bfe06a666b2d1a
rapgad <jeffreyhayford98@gamil.com>
2024-11-03 10:54:31 +0000

    Solving a 6kyu on codewars

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
-function deleteNth(arr,n){
+function happyCoding(nickname){
+    let value = 0 
+    switch(nickname.toLowerCase()){
+        case "happy coding":
+            value = 1
+            break
+        case "coding happy":
+            value = 2
+            break
+        case "coding everyday":
+            value = 3
+            break
+        case "happy programmer":
+            value = 4
+            break
+        case "unhappy codewarrior":
+            value = 5
+            break
+        default:
+            value = 5
 
-    let arrObj = arr.reduce((acc, curr)=>{
-        acc[curr] = (acc[curr] || 0) + 1
-
-        return acc
-    },{})
-    return Object.keys(arrObj).filter(num=> arrObj[num] <= n)
-
-    //return arrObj
-
-    // ...
-}
+        
+    }
+    return value
+    //Happy Coding ^_^
+    
+  }
 
 
+  console.log(happyCoding("Happy coding"))
 
-console.log(deleteNth([1,2,2,2,2,3,4],2))
+
